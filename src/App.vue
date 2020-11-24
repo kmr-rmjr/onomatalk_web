16c228ed307f02838b9fabbe48b5b302989d0144
kmr-rmjr <setoengr@gmail.com>
2020-11-24 22:29:01 +0900

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <translate></translate>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Translate from './components/pages/Translate.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    Translate,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/pages/Translate.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1>Hello World.</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      part: "",
+      activeColumn: null,
+      selectedWord: null,
+      languages: [{lang: '英語', code: 'en'}, {lang: 'フランス語', code: 'fr'}],
+      onomatopeLists: {
+        aColumn: {
+          label: 'あ',
+          words: [{word: 'イガイガ', explanation: '説明文'}, {word: 'ウズウズ', explanation: '説明文'}]
+        },
+        kColumn: {
+          label: 'か',
+          words: [{word: 'イガイガ', explanation: '説明文'}, {word: 'ウズウズ', explanation: '説明文'}]
+        },
+      }
+    }
+  },
+
+}
+</script>
+
+<style>
+  body {
+    font-family: Hiragino Sans,Meiryo,sans-serif;;
+  }
+</style>
